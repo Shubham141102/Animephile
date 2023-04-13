ae8472e19cbf85999ddcd888d2636fc543a270b3
IORD1 <pratham111ingole@gmail.com>
2023-04-14 00:26:17 +0530

    added a name to navbar

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import styles from "@/styles/Home.module.css"
-export default function Navbar (){
+import styles from "@/styles/Home.module.css";
+ 
+export default function Navbar (props){
+
+    function logoutme(){
+        props.logout();
+    }
+
     return (
         <div className={styles.container}>
             <nav>
                 <img src="logo.png" className={styles.logo}></img>
                 <input placeholder="Enter your Search" type='search' className={styles.search}/>
-                <img src="profile.jpg" className={styles.profile}></img>
+                <button className={styles.logoutbtn} onClick={logoutme}>LogOut</button>
+                <p className={styles.logoutbtn} >{props.displayname}</p>
+                <img src={props.profileurl} className={styles.profile}></img>
             </nav>
         </div>
     );
--- src/pages/components/loginpage.js
+++ src/pages/components/loginpage.js
@@ -1,65 +1,15 @@
 import styles from "@/styles/Loginstyle.module.css";
-import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider,signInWithPopup,signOut } from "firebase/auth";
 import { useState } from 'react'
 import Image from "next/image";
 import arrowsvg from './assests/arrow_forward_FILL0_wght400_GRAD0_opsz48.svg'
 export default function Login(props){
 
+  function func1(){
+    props.loginfunc();
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDehGE5mZ_mtlgTCjGopCMa8wzyx2aip6E",
-    authDomain: "animephile-a28c7.firebaseapp.com",
-    projectId: "animephile-a28c7",
-    storageBucket: "animephile-a28c7.appspot.com",
-    messagingSenderId: "269230750335",
-    appId: "1:269230750335:web:bd8b2a840d3d4305535d4a"
-  };
+  }
 
-  var a = true;
-  // Initialize Firebase
   
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  var name = {};
-  console.log(name);
-
-  function logmein(){
-    console.log("logging you in");
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      name = user;
-      console.log(user);
-      props.logfunc(0);
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      // const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-  }
-
-  function logmeout(){
-    signOut(auth).then(() => {
-  // Sign-out successful.
-  console.log("Logged Out");
-  props.logfunc(1);
-}).catch((error) => {
-  // An error happened.
-});
-  }
 
 
     return (
@@ -69,7 +19,7 @@ export default function Login(props){
                 <p className={styles.card__content}>Get Reminders for Lates episodes releases.
                 </p>
                 <div className={styles.card__form}>
-                    <button className={styles.signup} id="googlebtn" onClick={logmein}> Login with Google <Image src={arrowsvg} alt="->" height={22}  color="white"/></button>
+                    <button className={styles.signup} id="googlebtn" onClick={func1}> Login with Google <Image src={arrowsvg} alt="->" height={22}  color="white"/></button>
                 </div>
             </div>
         </div>
