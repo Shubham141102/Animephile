addeed00d9c37ff84da8f6f88b8bb1c2e798ff3e
hiteshjain48 <hiteshjainhd@gmail.com>
2023-05-08 00:31:57 +0530

    made api working

--- api.js
+++ api.js
@@ -1,4 +1,11 @@
 const jikanjs = require('@mateoaranda/jikanjs');
 res = async ()=>{return  await jikanjs.loadAnime(31240, 'episodes', 15);}
-data = res
-console.log(data)
+
+
+const getUser = async () => {
+
+    const response = await jikanjs.loadAnime(31240, 'episodes', 15);
+    console.log(response)
+}
+
+getUser()
