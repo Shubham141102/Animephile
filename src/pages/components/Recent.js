dd4d65674d55c71b3527f0f40f3de6ea08b9f259
hiteshjain48 <hiteshjainhd@gmail.com>
2023-05-09 15:34:41 +0530

    added scrolling button

--- src/pages/components/Recent.js
+++ src/pages/components/Recent.js
@@ -1,9 +1,9 @@
 import styles from "@/styles/Home.module.css"
 import Recentcard from "./Recentcard"
 import React from "react";
-import { useState, useEffect } from "react";
-import { ResponseCookies } from "next/dist/compiled/@edge-runtime/cookies";
-const jikanjs = require('@mateoaranda/jikanjs');
+import { useState, useEffect, useRef } from "react";
+
+
 export default function Recent(){
 
 
@@ -11,18 +11,67 @@ export default function Recent(){
         getUser();
       }, [])
 
-
-    var res = {}
-
     const [state, setState] =  useState([])
     const getUser = async () => {
-        // const response = await jikanjs.loadAnime(31240, 'episodes', 15);
         const response = await fetch(`https://api.jikan.moe/v4/top/anime`).then(
             (res) => res.json()
           );
-        // res = await response.json()
         setState(response.data);
     }
+
+
+
+    const scrollRef = useRef(null);
+    const [scrollLeft, setScrollLeft] = useState(0);
+    const [scrollInterval, setScrollInterval] = useState(null);
+    const [isHovering, setIsHovering] = useState(false);
+
+    useEffect(() => {
+        // start scrolling automatically
+        const interval = setInterval(() => {
+        setScrollLeft(scrollLeft => scrollLeft + 1);
+        }, 30);
+
+        setScrollInterval(interval);
+
+        // clean up interval on unmount
+        return () => clearInterval(interval);
+    }, []);
+
+    const handleScroll = event => {
+        // update scrollLeft state when scrolling
+        setScrollLeft(event.target.scrollLeft);
+    };
+
+    const handlePauseScroll = () => {
+        // pause scrolling on hover
+        clearInterval(scrollInterval);
+        setIsHovering(true);
+    };
+
+    const handleResumeScroll = () => {
+        // resume scrolling on hover out
+        const interval = setInterval(() => {
+        setScrollLeft(scrollLeft => scrollLeft + 1);
+        }, 30);
+
+        setScrollInterval(interval);
+        setIsHovering(false);
+    };
+
+    const handleScrollLeft = () => {
+        // scroll left on button click
+        scrollRef.current.scrollLeft -= 600;
+    };
+
+    const handleScrollRight = () => {
+        // scroll right on button click
+        scrollRef.current.scrollLeft += 600;
+    };
+
+
+
+
     const cards = state.map(item => {
         return (
             <Recentcard item={item}/>
@@ -31,11 +80,27 @@ export default function Recent(){
         
         return(
             <>
-            <h1 className={styles.sectionheading}>Top trending</h1>
-            <div className={styles.recentcontainer}>
-               {cards}
-               {/* <Recentcard item={state}/> */}
-            </div>
+                <h1 className={styles.sectionheading}>Top trending</h1>
+                <div className={styles.container_recent}>
+                    <div 
+                    className={styles.recentcontainer}
+                    ref={scrollRef}
+                    onScroll={handleScroll}
+                    onMouseEnter={handlePauseScroll}
+                    onMouseLeave={handleResumeScroll}
+                    >
+                    {cards}
+                    {/* <Recentcard item={state}/> */}
+                    </div>
+                    <div className={styles.buttons}>
+                        <button onClick={handleScrollLeft} disabled={isHovering || scrollLeft <= 0}>
+                        {'<'}
+                        </button>
+                        <button onClick={handleScrollRight} >
+                        {'>'}
+                        </button>
+                    </div>
+                </div>
             </>
         )
     }
--- src/pages/components/Recentcard.jsx
+++ src/pages/components/Recentcard.jsx
@@ -12,61 +12,28 @@ export default  function Recentcard(props){
         background: `url(${url}) no-repeat`,
     }
    
-    
-
     return(
-        // <div className={styles.recentcardbackground} id="back" >
-        //     <div className={styles.recentcard} onClick={popup}>
-                
-        //         <div className={styles.cardblur}>
-        //             <div className={styles.cardhead}>
-        //                 <p className={styles.cardheader}>{props.item.title}</p>
-        //                 <p className={styles.cardsubheader}>{props.item.mal_id}</p>
-        //             </div>
-        //             <div className={styles.cardplay}>
-        //                 <div className={styles.playbutton}>
-        //                     <img src="play.svg" alt="" style={{cursor : 'pointer'}} />
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        // <div className={styles.accordion}>
-        //     <ul className={styles.accordionul}>
-        //         <li className={styles.accordionli}>
-        //             <div className={styles.image_title}>
-        //                 <a className={styles.image_titlea}href="#">{props.item.title}</a>
-        //             </div>
-        //             <a href="https://ibb.co/gGv6QS"><img className={styles.accordionimg}src="https://image.ibb.co/k7P0kS/transformers4_640x320.jpg" alt="transformers4_640x320" border="0"></img></a>
-        //         </li>
-        //     </ul>
-        // </div>
+        <div className={style.recentcard}>
+            <div className={styles.card}>
+                <div className={styles.card__inner} style={myStyle}>
+                    <main className={styles.card__body}>
+                    
+                    <div className={styles.card__info}>
+                        <h1 className={styles.card__title}>{props.item.title}</h1>
+                    
+                        <p className={styles.card__slug}>{props.item.synopsis.slice(0,200)}</p>
+                    
+                        <a href={props.item.trailer.url}>
+                            <button className={styles.card__btn} value="Watch trailer" >Watch trailer</button>
+                        </a>
 
-   
-    <div className={style.recentcard}>
-        <div className={styles.card}>
-                {/* <div className={styles.card__img}>
-                    <h1>hitesh</h1>
-                </div> */}
-            <div className={styles.card__inner} style={myStyle}>
-                <main className={styles.card__body}>
-                
-                <div className={styles.card__info}>
-                    <h1 className={styles.card__title}>{props.item.title}</h1>
-                
-                    <p className={styles.card__slug}>{props.item.synopsis.slice(0,200)}</p>
-                
-                    <a href={props.item.trailer.url}>
-                        <button className={styles.card__btn} value="Watch trailer" >Watch trailer</button>
-                    </a>
-
-                    <div className={styles.card__rating}>
-                        {props.item.score}
+                        <div className={styles.card__rating}>
+                            {props.item.score}
+                        </div>
                     </div>
+                    </main>
                 </div>
-                </main>
             </div>
         </div>
-    </div>
     )
 }
