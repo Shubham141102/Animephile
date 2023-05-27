ec3edbd6001cd7528b15a936e95fbdc3580b861d
IORD1 <pratham111ingole@gmail.com>
2023-05-27 12:25:46 +0530

    added more data to airringtoday

--- src/pages/components/AiringCard.js
+++ src/pages/components/AiringCard.js
@@ -19,17 +19,27 @@ export default  function AiringCard(props){
         <div className={style.recentcard}>
             <div className={styles.card}>
                 <div className={styles.card__inner} style={myStyle}>
-                    <div className={styles.filter}></div>
+                    {/* <div className={styles.filter}></div> */}
                 </div>
                 <main className={styles.card__body}>
                     <div className={styles.card__info}>
                         <h1 className={styles.card__title}>{(props.item.title_english) ? props.item.title_english : props.item.title}</h1>
                         <button onClick={() => {followit()}} className={styles.card__btn2} value="follow" >Follow</button>
-                        
+                        <a href={props.item.trailer.url} target="_blank">
+                            <button className={styles.card__btn} value="Watch trailer" >Watch trailer</button>
+                        </a>
 
                         <div className={styles.card__rating}>
-                            Broadcast    : {props.item.broadcast.string} (3:30 hrs ahead of IST)
+                            <strong>Broadcast  </strong>  : {props.item.broadcast.string} (3:30 hrs ahead of IST)
                         </div>
+                        <div className={styles.card__rating}>
+                            <strong>Current Episode  </strong>  : {props.item.episodes} 
+                        </div>
+                        {/* <div className={styles.card__rating}>
+                            {props.item.genres[0].name} 
+                        </div> */}
+                        {console.log(props.item.genres[0].)}
+
                     </div>
                 </main>
             
--- src/pages/components/Airingtoday.js
+++ src/pages/components/Airingtoday.js
@@ -21,6 +21,7 @@ export default function Airingtoday(props){
             (res) => res.json()
           );
         setState(response.data);
+        console.log(response.data);
     }
 
 
